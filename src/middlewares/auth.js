e97ca82cfd9517394e284a05229c3b2f2ea2e193
harshat <harshat@futuresoftinida.com>
2026-02-02 22:52:32 +0530

    changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,45 +1,52 @@
-import jwt from 'jsonwebtoken'
-import { apiHandler } from '../utils/index.js';
+import jwt from "jsonwebtoken";
+import { apiHandler } from "../utils/index.js";
 
 export const validateAccessToken = async (req, res, next) => {
-    const authorizationHeader = req.headers.authorization;
-    if (authorizationHeader) {
-        const token = authorizationHeader.split(" ")[1]
-        let data;
-        try {
-            data = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET)
-
-
-        } catch (error) {
-            if (error.type === "expired" || error.type === "invalid") {
-                return apiHandler.sendUnauthorizedError(
-                    res,
-                    null,
-                    error.type === "expired" ? "Token expired" : "Invalid Token"
-                );
-            } else {
-                return apiHandler.sendUnauthorizedError(res, null, "Authentication Error");
-            }
-        }
-
-
-        req.auth = data
-        next()
+  const authorizationHeader = req.headers.authorization;
+  if (authorizationHeader) {
+    const token = authorizationHeader.split(" ")[1];
+    let data;
+    try {
+      data = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
+    } catch (error) {
+      if (error.type === "expired" || error.type === "invalid") {
+        return apiHandler.sendUnauthorizedError(
+          res,
+          null,
+          error.type === "expired" ? "Token expired" : "Invalid Token",
+        );
+      } else {
+        return apiHandler.sendUnauthorizedError(
+          res,
+          null,
+          "Authentication Error",
+        );
+      }
     }
-    else {
-        return apiHandler.sendUnauthorizedError(res, null, "Token is missing");
-    }
-}
-// ES6 module syntax
-export const authorizeRoles = (...allowedRoles) => {
 
-
-    return (req, res, next) => {
-        console.log(allowedRoles);
-
-        if (!req.auth || !allowedRoles.includes(req.auth.role)) {
-            return res.status(403).json({ message: 'Access denied: insufficient permissions.' });
-        }
-        next();
-    };
+    req.auth = data;
+    next();
+  } else {
+    return apiHandler.sendUnauthorizedError(res, null, "Token is missing");
+  }
 };
+// ES6 module syntax
+const authorizePermission = (resource, action) => {
+  return (req, res, next) => {
+    const permissions = req.user.permissions;
+
+    // Resource not present
+    if (!permissions || !permissions[resource]) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    // Action not allowed
+    if (!permissions[resource].includes(action)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    next();
+  };
+};
+
+module.exports = authorizePermission;
